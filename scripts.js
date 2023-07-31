652f47f9a9f687d3b65a2a6d5dd28ecda7db3fd5
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2023-07-31 20:57:57 +0530

    Create the game function.

--- scripts.js
+++ scripts.js
@@ -14,24 +14,25 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-	let playerInput = playerSelection.charAt(0).toUpperCase() + 
-		playerSelection.slice(1).toLowerCase();
+	if (playerSelection === null) {
+		return 'The player cancelled the game.'
+	}
 
-	if (playerInput === computerSelection) {
-		return 'It\'s a Draw. Nobody Wins.';
-	} else if (playerInput === 'Rock') {
+	if (playerSelection === computerSelection) {
+		return 'It\'s a Draw. Nobody Wins';
+	} else if (playerSelection === 'Rock') {
 		if (computerSelection === 'Scissors') {
 			return 'You Win! Rock beats Scissors'
 		} else {
 			return 'You Lose! Paper beats Rock'
 		}
-	} else if (playerInput === 'Paper') {
+	} else if (playerSelection === 'Paper') {
 		if (computerSelection === 'Rock') {
 			return 'You Win! Paper beats Rock'
 		} else {
 			return 'You Lose! Scissors beat Paper'
 		}
-	} else if (playerInput === 'Scissors') {
+	} else if (playerSelection === 'Scissors') {
 		if (computerSelection === 'Paper') {
 			return 'You Win! Scissors beat Paper'
 		} else {
@@ -39,3 +40,52 @@ function playRound(playerSelection, computerSelection) {
 		}
 	}
 }
+
+function getPlayerChoice() {
+	let input = prompt('Type your weapon.');
+	if (input === null) {
+		return input;
+	}
+	let standardInput = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+	switch (standardInput) {
+		case 'Rock':
+		case 'Paper':
+		case 'Scissors':
+			return standardInput;
+		default:
+			console.log(`There is a misinput. The input was: ${input}.`);
+			return getPlayerChoice();
+	}
+}
+
+function game() {
+	let computerScore = 0;
+	let playerScore = 0;
+	for (i = 0; i < 5; i++) {
+		let outcome = playRound(getPlayerChoice(), getComputerChoice());
+		if (outcome.includes('You Win!')) {
+			playerScore++;
+		} else if (outcome.includes('You Lose!')) {
+			computerScore++;
+		} else if (outcome.includes('cancelled')) {
+			console.log(outcome);
+			return false;
+		}
+		console.log(outcome);
+	}
+
+	if (playerScore > computerScore) {
+		let scoreDifference = playerScore - computerScore;
+		console.log(`The player has Won by ${scoreDifference}\
+		 round${scoreDifference > 1 ? 's' : ''}!`);
+		return true;
+	} else if (playerScore < computerScore) {
+		let scoreDifference = computerScore - playerScore;
+		console.log(`The computer Wins by ${scoreDifference}\
+		round${scoreDifference > 1 ? 's' : ''}!`);
+		return true;
+	} else {
+		console.log('It was a Draw!');
+		return true;
+	}
+ }
