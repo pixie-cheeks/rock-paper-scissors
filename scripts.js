a0e18ff34eef62e81ac130e24b5eca3ea3e58633
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2023-07-31 19:28:23 +0530

    Create the playRound function.

--- scripts.js
+++ scripts.js
@@ -2,15 +2,40 @@ function getComputerChoice() {
 	let randomInt = Math.ceil(Math.random() * 3);
 	switch (randomInt) {
 		case 1:
-			console.log('Rock');
+			return 'Rock';
 			break;
 		case 2:
-			console.log('Paper');
+			return 'Paper';
 			break;
 		case 3:
-			console.log('Scissors')
+			return 'Scissors';
 			break;
 	}
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+	let playerInput = playerSelection.charAt(0).toUpperCase() + 
+		playerSelection.slice(1).toLowerCase();
+
+	if (playerInput === computerSelection) {
+		return 'It\'s a Draw. Nobody Wins.';
+	} else if (playerInput === 'Rock') {
+		if (computerSelection === 'Scissors') {
+			return 'You Win! Rock beats Scissors'
+		} else {
+			return 'You Lose! Paper beats Rock'
+		}
+	} else if (playerInput === 'Paper') {
+		if (computerSelection === 'Rock') {
+			return 'You Win! Paper beats Rock'
+		} else {
+			return 'You Lose! Scissors beat Paper'
+		}
+	} else if (playerInput === 'Scissors') {
+		if (computerSelection === 'Paper') {
+			return 'You Win! Scissors beat Paper'
+		} else {
+			return 'You Lose! Rock beats Scissors'
+		}
+	}
+}
